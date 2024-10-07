cba1222c73a8a9bbf3d1a7d51839ff1438a8069a
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2024-10-07 08:50:36 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, TextInput, Button, Alert } from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+  const gradePoints = {'F': 0,'D': 1.5,'C': 2,'C+': 2.75, 'B': 3, 'B+': 3.5,  'A': 4 };
+  var totalGradeScores = 0;
+  const [sswd, setSswd] = useState('D');
+  const [ob, setOb] = useState('D');
+  
+  var gpa=0;
+  var credits=5;
+  var totalPossibleCredits = 10;
+  
+  function clickMe(){
+    alert("this is the click me button"); //alert for web
+    Alert.alert("this is the click me button"); //alert for phone
+    //get the studnet's gradePointsfor SSWD, multiply it by the credits 
+    //add the result to totalGradeScores as an accumulator variable (e.g. tgs=tgs+sswd)
+    
+    //get the student's gradePointsfor Ob, multiply it by the credits
+    //add the result to totalGradeScores as an accumulator variable (e.g. tgs=tgs+ob)
+   
+    //calculate the gpa as the totalsGradeScores divided by the totalPossibleCredits
+    //Output the calculated GPA result to the user using an alert (you must concatenate the gpa)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Maths</Text>
+        <TextInput placeholder="Grade" onChangeText={(val) => setSswd(val)}/>
+      </View>
+      <View>
+        <Text >Organisational Behaviour</Text>
+        <TextInput placeholder="Grade" onChangeText={(val) => setOb(val)}/>
+      </View>
+      <Button title="submit" onPress={clickMe}/>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
